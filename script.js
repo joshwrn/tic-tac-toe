f020244ff42e810e43bcde08eb7e3f992c990b85
joshwrn <joshnwarren@gmail.com>
2021-04-22 17:08:35 -0700

    finished i believe

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const switchMode = (function () {
 })();
 
 const gameboardModule = (function () {
-  let gameBoard = [];
+  let gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
   const winningCells = [
     [0, 1, 2],
     [5, 4, 3],
@@ -31,39 +31,67 @@ const gameboardModule = (function () {
     [6, 4, 2],
   ];
 
-  // div id's
-
   console.log(winningCells);
 
+  // player winner checker
   const checkArrays = function (arr2) {
     for (i = 0; i < 8; i++) {
-      console.log(winningCells[i]);
       if (winningCells[i].every((val) => arr2.includes(val))) {
-        return "trusies";
+        return "win";
       }
     }
   };
 
-  console.log("check test " + checkArrays([4, 2, 6]));
+  //bot move checker
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  let randomNumber;
+  let newMove;
+  let botArray = [];
+
+  const chooseRandomMove = function () {
+    newRandom = getRandomInt(gameBoard.length);
+    randomNumber = gameBoard[newRandom];
+    console.log(`spliced ${randomNumber}`);
+    botArray.push(randomNumber);
+    gameBoard.splice(newRandom, 1);
+  };
 
   //function for clicking on a cell
   const chooseCell = (function () {
+    //document elements
+    const root = document.documentElement;
+
+    const roundStatusText = document.getElementById("round-status-text");
     // variables
     let playerArray = [];
-    let pcArray = [];
+
+    let botRandomMove = [];
+    let gameOver = false;
     // choose symbol
     let playerChoice = "";
+    let botChoice = "";
     const choosePlayer = (function () {
       document.addEventListener("click", (e) => {
         if (e.target.matches(".select-button")) {
           playerChoice = e.target.getAttribute("data-player-selection");
           console.log("player chose " + playerChoice);
           //hide buttons
-          const root = document.documentElement;
           root.style.setProperty("--select-hidden", "hidden");
+          // choose bot symbol
+          if (playerChoice == "X") {
+            botChoice = "O";
+          } else {
+            botChoice = "X";
+          }
+          console.log(`bot chose ${botChoice}`);
         }
       });
     })();
+    //restart
+
     // choose cell
     document.addEventListener("click", (e) => {
       //if target matches cell hasnt been given chosen class yet and player has picked symbol run function
@@ -71,7 +99,8 @@ const gameboardModule = (function () {
         e.target.matches(".cell") &&
         !e.target.matches(".chosen-cell") &&
         playerChoice != "" &&
-        playerArray.length < 5
+        playerArray.length < 5 &&
+        gameOver == false
       ) {
         const dataCell = e.target.getAttribute("data-cell");
         console.log("cell " + dataCell + " clicked");
@@ -83,49 +112,42 @@ const gameboardModule = (function () {
           _createChoice.setAttribute("id", dataCell);
           _selectedCell.appendChild(_createChoice);
           _selectedCell.classList.add("chosen-cell");
-          //add cell to player's array
-          playerArray.push(e.target.getAttribute("data-cell"));
-          console.log(playerArray);
-          console.log(zero);
-          if (
-            (zero.textContent.includes(playerChoice) &&
-              one.textContent.includes(playerChoice) &&
-              two.textContent.includes(playerChoice)) ||
-            (three.textContent.includes(playerChoice) &&
-              four.textContent.includes(playerChoice) &&
-              five.textContent.includes(playerChoice)) ||
-            (six.textContent.includes(playerChoice) &&
-              seven.textContent.includes(playerChoice) &&
-              eight.textContent.includes(playerChoice)) ||
-            (zero.textContent.includes(playerChoice) &&
-              three.textContent.includes(playerChoice) &&
-              six.textContent.includes(playerChoice)) ||
-            (one.textContent.includes(playerChoice) &&
-              four.textContent.includes(playerChoice) &&
-              seven.textContent.includes(playerChoice)) ||
-            (two.textContent.includes(playerChoice) &&
-              five.textContent.includes(playerChoice) &&
-              eight.textContent.includes(playerChoice)) ||
-            (zero.textContent.includes(playerChoice) &&
-              four.textContent.includes(playerChoice) &&
-              eight.textContent.includes(playerChoice)) ||
-            (six.textContent.includes(playerChoice) &&
-              four.textContent.includes(playerChoice) &&
-              two.textContent.includes(playerChoice))
-          ) {
-            const zero = document.getElementById("0");
-            const one = document.getElementById("1");
-            const two = document.getElementById("2");
-            const three = document.getElementById("3");
-            const four = document.getElementById("4");
-            const five = document.getElementById("5");
-            const six = document.getElementById("6");
-            const seven = document.getElementById("7");
-            const eight = document.getElementById("8");
-            console.log("you win");
+          //add cell to player's array and remove from gameboard
+          playerArray.push(parseInt(e.target.getAttribute("data-cell")));
+          gameBoard.splice(gameBoard.indexOf(parseInt(dataCell)), 1);
+          console.log(`player spliced ${gameBoard}`);
+          //check for win
+          if (checkArrays(playerArray) == "win") {
+            gameOver = true;
+            root.style.setProperty("--restart-hidden", "visible");
+            roundStatusText.appendChild(document.createTextNode("YOU WIN"));
+          } else {
+            chooseRandomMove();
+            let botSelectedCell = document.querySelector(
+              `[data-cell="${randomNumber}"]`
+            );
+
+            console.log(gameBoard);
+            let createBotChoice = document.createElement("p");
+            let addBotText = document.createTextNode(botChoice);
+            createBotChoice.appendChild(addBotText);
+            createBotChoice.setAttribute("id", randomNumber);
+            botSelectedCell.appendChild(createBotChoice);
+            botSelectedCell.classList.add("chosen-cell");
+
+            if (checkArrays(botArray) == "win") {
+              gameOver = true;
+              root.style.setProperty("--restart-hidden", "visible");
+              roundStatusText.appendChild(document.createTextNode("BOT WINS"));
+            }
           }
         })();
       }
     });
   })();
 })();
+
+const restartButton = document.getElementById("restart-button");
+restartButton.onclick = function () {
+  location.reload();
+};
